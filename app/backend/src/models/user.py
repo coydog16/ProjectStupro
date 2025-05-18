bbfef2a2b04000ab4960034039c9feee7e288acb
coydog16 <coydog16@gmail.com>
2025-05-18 07:50:40 +0000

    userData

--- app/backend/src/models/user.py
+++ app/backend/src/models/user.py
@@ -1,6 +1,12 @@
-from datetime import datetime
+from datetime import datetime, timezone, timedelta
+import pytz
 from werkzeug.security import generate_password_hash, check_password_hash
 from src.core.database import db
+
+JST = pytz.timezone('Asia/Tokyo')
+
+def get_jst_now():
+    return datetime.noe(pytz.UTC).astimezone(JST)
 
 class User(db.Model):
     __tablename__ = 'users'
@@ -8,3 +14,8 @@ class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(50), unique=True, nullable=False)
     email = db.Column(db.String(50), unique=True, nullable=False)
+    password_hash = db.Column(db.String(255), nullable=False)
+    is_active = db.Column(db.Boolean, default=True)
+    create_at = db.Column(db.DateTime, default=get_jst_now)
+
+
